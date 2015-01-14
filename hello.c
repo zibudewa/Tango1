811178cb2e023df1b4577e5688b719a35180c285
Zibusiso Dewa <P13212712@GH584-009.lec-admin.dmu.ac.uk>
2015-01-14 09:22:08 +0000

    basic hello world program

---
+++ hello.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+int main()
+{
+    int x = 3;
+    printf ("The value of x is %i\n", x);
+    return 0;
+}
+#include <stdio.h>
+
+int main()
+{
+    printf ("\t***\n");
+    printf ("\t***\n");
+    printf ("\t***\n");
+    return 0;
+}
+*/
+#include<stdio.h>
+int main ()
+{
+printf ("hello world")
+return 0;
+}
