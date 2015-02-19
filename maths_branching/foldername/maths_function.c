26049fa6b0ea47099208f1cbb8c91c09842964b5
Zibusiso Dewa <P13212712@GH584-009.lec-admin.dmu.ac.uk>
2015-02-19 18:38:13 +0000

    maths function total of three values

---
+++ maths_branching/foldername/maths_function.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{  
+          int a, b, c, d;
+          printf("Enter two numbers\n");
+          scanf("%d%d%d", &a, &b);
+
+          d = a + b + c;
+          c = a - b;
+ 
+          
+          printf("sum of numbers = %d\n", c);
+          
+          return 0;
+}
+
