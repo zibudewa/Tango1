3481f0158bf06514e2f3bc9b8ce7edfedcbdbd31
Zibusiso Dewa <P13212712@GH584-009.lec-admin.dmu.ac.uk>
2015-02-19 18:13:21 +0000

    maths function branch 1st branch

---
+++ maths_branching/foldername/maths_function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{  
+          int a, b, c;
+          printf("Enter two numbers\n");
+          scanf("%d%d", &a, &b);
+
+          c = a - b; 
+          
+          printf("sum of numbers = %d\n", c);
+          
+          return 0;
+}
+
